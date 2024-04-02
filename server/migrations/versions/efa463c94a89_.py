89971fafe61e86bc79416a21755f16af810a18ea
Erin Wise <erinwise88@gmail.com>
2024-04-02 13:14:29 -0400

    updates

---
+++ server/migrations/versions/efa463c94a89_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: efa463c94a89
+Revises: 
+Create Date: 2024-04-02 12:31:10.979502
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'efa463c94a89'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
